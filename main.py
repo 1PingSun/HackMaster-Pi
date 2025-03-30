301956ba8122085caf4ab21ad91187110c4fd0db
piinng <52sunyiping@gmail.com>
2025-03-30 23:59:23 +0800

    2025-03-30 Add wordlist generator front-end page

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def read_root(request: Request):
         {"request": request, "message": "Hello, World!"}
     )
 
+@app.get("/Wi-Fi/wordlist-generator", response_class=HTMLResponse)
+def read_wordlist_generator(request: Request):
+    return templates.TemplateResponse(
+        "Wi-Fi/wordlist-generator.html", 
+        {"request": request, "message": "Wordlist Generator"}
+    )
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=4000)
